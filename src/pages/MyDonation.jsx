01dded4e0a40d68801a844558a194e54e90341e6
mohammad firmansyah <firmansmoh842@gmail.com>
2023-12-09 12:15:17 +0700

    finish page donation

--- src/pages/MyDonation.jsx
+++ src/pages/MyDonation.jsx
@@ -1,41 +1,57 @@
 import Nav from "../components/Nav"
+import Footer from "../components/Footer"
 
 export default function MyDonation() {
     return (
         <>
 
-            <div className='bg-gradient-to-b from-white to-indigo-100 '>
+            <div className='bg-gradient-to-b from-indigo-5  0 to-white'>
             <Nav/>
             
+            <div className="flex w-full  p-8 h-[556px]">
 
-            <div className="flex-column">
+            
+                <div className="flex-column">
 
-                <div className='p-8  '>
-                
-                    <p className="font-semibold">Akun <span className="mx-3"> > </span>   Donasi Saya</p>
-                </div>
+                    <div className=''>
+                        <p className="font-semibold">Akun <span className="mx-3"> > </span>   Donasi Saya</p>
+                    </div>
 
-                <div className="px-8 ">
-                    <div className="inline-block bg-white w-[190px] h-[94px] rounded-md border-2 border-black">
-                        
-                        <ul className="p-2 ">
-                            <li className="hover:bg-gray-100 p-2">
-                                <a>
-                                    Edit Profil
-                                    </a>
-                                    
-                            </li>
-                            <li className="hover:bg-gray-100 p-2">
-                                <a>
-                                    Donasi Saya
-                                    </a>
-                                    
-                            </li>
-                        </ul>
+                    <div className="mt-6">
+                        <div className="inline-block bg-white w-[190px] h-[94px] rounded-md border-2 border-black">
+                            
+                            <ul className="p-2 ">
+                                <li className="hover:bg-gray-100 p-2">
+                                    <a>
+                                        Edit Profil
+                                        </a>
+                                        
+                                </li>
+                                <li className="hover:bg-gray-100 p-2">
+                                    <a>
+                                        Donasi Saya
+                                        </a>
+                                        
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    </div>
+
+                    <div className="flex-column ml-6">
+                        <h1 className="font-bold text-xl">Donasi Saya</h1>
+
+                        <p className="font-regular text-medium mt-[21px]">
+                            Anda belum pernah berdonasi. Apakah kamu tertarik untuk memulainya? 
+                        </p>
+
+                        <button className="py-2 px-7 mt-[21px] bg-indigo-900 text-white font-semi rounded-full text-sm">Donasi Sekarang</button>
                     </div>
                 </div>
 
-                </div>
+                
+            <Footer/>
             </div>
         
         </>
